bc448c0cdd6601b901e43f44a6745dffbe8a6632
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-07-29 07:30:42 -0400

    Working on project skeleton

---
+++ EditUITests/EditUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EditUITestsLaunchTests.swift
+//  EditUITests
+//
+//  Created by Matthew Massicotte on 2023-07-29.
+//
+
+import XCTest
+
+final class EditUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
