8f4d58889b50cd074894300680ba306c59a51983
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-12-06 06:24:07 -0500

    Use Glyph for text presentation attributes

--- Edit/Modules/TextSystem/TextPresentation.swift
+++ Edit/Modules/TextSystem/TextPresentation.swift
@@ -1,7 +1,7 @@
 import Foundation
 
+import Glyph
 import NSUI
-import Rearrange
 
 public struct TextPresentation {
 	public let applyRenderingStyle: ([NSAttributedString.Key : Any], NSRange) -> Void
@@ -10,44 +10,9 @@ public struct TextPresentation {
 extension TextPresentation {
 	@MainActor
 	public init(textView: NSUITextView) {
-		if let textLayoutManager = textView.textLayoutManager {
-			self.init(textLayoutManager: textLayoutManager, textView: textView)
-		} else {
-			self.init(layoutManager: textView.nsuiLayoutManager!)
-		}
-	}
-	
-	@MainActor
-	public init(textLayoutManager: NSTextLayoutManager, textView: NSUITextView) {
 		self.init(
 			applyRenderingStyle: { attrs, range in
-				guard
-					let contentManager = textLayoutManager.textContentManager,
-					let textRange = NSTextRange(range, provider: contentManager)
-				else {
-					return
-				}
-
-				let selection = textView.selectedRanges
-
-				textLayoutManager.setRenderingAttributes(attrs, for: textRange)
-
-				textView.selectedRanges = [NSValue(range: range)]
-
-				textView.selectedRanges = selection
-			}
-		)
-	}
-
-	@MainActor
-	public init(layoutManager: NSLayoutManager) {
-		self.init(
-			applyRenderingStyle: { attrs, range in
-#if os(macOS)
-				layoutManager.setTemporaryAttributes(attrs, forCharacterRange: range)
-#else
-				print("unsupported rendering style")
-#endif
+				textView.setRenderingAttributes(attrs, for: range)
 			}
 		)
 	}
